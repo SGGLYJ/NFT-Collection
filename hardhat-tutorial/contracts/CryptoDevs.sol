5b6bdf18bf5a4e3951d36e7c51da8e5798e9281a
SGGLYJ <lg250712@gmail.com>
2023-05-25 16:27:42 +0900

    initial commit

---
+++ hardhat-tutorial/contracts/CryptoDevs.sol
@@ -0,0 +1,126 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./IWhitelist.sol";
+
+contract CryptoDevs is ERC721Enumerable, Ownable {
+
+    //ABC_1, ABC_2, ABC_3, ABC_4 에서 ABC_ 이게 baseTokenURI임
+    /**
+      * @dev _baseTokenURI for computing {tokenURI}. If set, the resulting URI for each
+      * token will be the concatenation of the `baseURI` and the `tokenId`.
+      */
+    string _baseTokenURI;
+
+    // 가격임
+    //  _price is the price of one Crypto Dev NFT
+    uint256 public _price = 0.01 ether;
+
+    // 긴급상황 시 paused를 true로 바꾸면 민팅중인 로직 정지 가능(이건 컨트렉트 오너만 호출 가능)
+    // _paused is used to pause the contract in case of an emergency
+    bool public _paused;
+
+    // 현재 민팅한 토큰 개수를 저장해두고 최댓값 제한
+    // max number of CryptoDevs
+    uint256 public maxTokenIds = 20;
+
+    // total number of tokenIds minted
+    uint256 public tokenIds;
+
+    // Whitelist contract instance
+    IWhitelist whitelist;
+
+    //프리세일 시간 지정 가능
+    // boolean to keep track of whether presale started or not
+    bool public presaleStarted;
+
+    //프리세일 끝(퍼블릭세일 시작?)
+    // timestamp for when presale would end
+    uint256 public presaleEnded;
+
+    modifier onlyWhenNotPaused {
+        require(!_paused, "Contract currently paused");
+        _;
+    }
+
+    /**
+      * @dev ERC721 constructor takes in a `name` and a `symbol` to the token collection.
+      * name in our case is `Crypto Devs` and symbol is `CD`.
+      * Constructor for Crypto Devs takes in the baseURI to set _baseTokenURI for the collection.
+      * It also initializes an instance of whitelist interface.
+      */
+    constructor (string memory baseURI, address whitelistContract) ERC721("Crypto Devs", "CD") {
+        _baseTokenURI = baseURI;
+        whitelist = IWhitelist(whitelistContract);
+    }
+
+    /**
+    * @dev startPresale starts a presale for the whitelisted addresses
+      */
+    function startPresale() public onlyOwner {
+        presaleStarted = true;
+        // Set presaleEnded time as current timestamp + 5 minutes
+        // Solidity has cool syntax for timestamps (seconds, minutes, hours, days, years)
+        presaleEnded = block.timestamp + 5 minutes;
+    }
+
+    /**
+      * @dev presaleMint allows a user to mint one NFT per transaction during the presale.
+      */
+    function presaleMint() public payable onlyWhenNotPaused {
+        require(presaleStarted && block.timestamp < presaleEnded, "Presale is not running");
+        require(whitelist.whitelistedAddresses(msg.sender), "You are not whitelisted");
+        require(tokenIds < maxTokenIds, "Exceeded maximum Crypto Devs supply");
+        require(msg.value >= _price, "Ether sent is not correct");
+        tokenIds += 1;
+        //_safeMint is a safer version of the _mint function as it ensures that
+        // if the address being minted to is a contract, then it knows how to deal with ERC721 tokens
+        // If the address being minted to is not a contract, it works the same way as _mint
+        _safeMint(msg.sender, tokenIds);
+    }
+
+    /**
+    * @dev mint allows a user to mint 1 NFT per transaction after the presale has ended.
+    */
+    function mint() public payable onlyWhenNotPaused {
+        require(presaleStarted && block.timestamp >=  presaleEnded, "Presale has not ended yet");
+        require(tokenIds < maxTokenIds, "Exceed maximum Crypto Devs supply");
+        require(msg.value >= _price, "Ether sent is not correct");
+        tokenIds += 1;
+        _safeMint(msg.sender, tokenIds);
+    }
+
+    /**
+    * @dev _baseURI overrides the Openzeppelin's ERC721 implementation which by default
+    * returned an empty string for the baseURI
+    */
+    function _baseURI() internal view virtual override returns (string memory) {
+        return _baseTokenURI;
+    }
+
+    /**
+    * @dev setPaused makes the contract paused or unpaused
+      */
+    function setPaused(bool val) public onlyOwner {
+        _paused = val;
+    }
+
+    /**
+    * @dev withdraw sends all the ether in the contract
+    * to the owner of the contract
+      */
+    function withdraw() public onlyOwner  {
+        address _owner = owner();
+        uint256 amount = address(this).balance;
+        (bool sent, ) =  _owner.call{value: amount}("");
+        require(sent, "Failed to send Ether");
+    }
+
+      // Function to receive Ether. msg.data must be empty
+    receive() external payable {}
+
+    // Fallback function is called when msg.data is not empty
+    fallback() external payable {}
+}
